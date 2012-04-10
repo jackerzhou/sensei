eaa2a5ce235ebcbf99f8c6eefa39bc9b3cda22f4
wangdong <wangdong@xueqiu.com>
2012-04-10 11:14:46 +0800

    add custom filter constructor params

--- sensei-core/src/main/java/com/senseidb/search/query/filters/CustomFilterConstructor.java
+++ sensei-core/src/main/java/com/senseidb/search/query/filters/CustomFilterConstructor.java
@@ -1,6 +1,9 @@
 package com.senseidb.search.query.filters;
 
+import java.lang.reflect.Constructor;
+
 import org.apache.lucene.search.Filter;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class CustomFilterConstructor extends FilterConstructor
@@ -12,6 +15,10 @@ public class CustomFilterConstructor extends FilterConstructor
 //  // custom
 //  "custom" : {
 //    "class" : "com.sensidb.query.TestFilter"
+// 	  "params": [Long]
+//	  "values": [9220789803] 
+// 
+//	  
 //  }
   
   @Override
@@ -19,10 +26,25 @@ public class CustomFilterConstructor extends FilterConstructor
   {
     try
     {
-      String className = ((JSONObject)json).getString(CLASS_PARAM);
+      JSONObject jsonObject = (JSONObject) json; 
+      String className = jsonObject.getString(CLASS_PARAM);
+      
+      JSONArray paramArray = jsonObject.getJSONArray(PARAMS_PARAM);
+      Class[] params = new Class[paramArray.length()];
+      for(int i = 0; i < paramArray.length(); i++) {
+    	  params[i] = Class.forName((String)paramArray.get(i));
+      }
+      
+      JSONArray valueArray = jsonObject.getJSONArray(VALUES_PARAM);
+      Object[] values = new Object[valueArray.length()];
+      for(int i = 0; i < valueArray.length(); i++) {
+    	  values[i] = params[i].cast(valueArray.get(i));
+      }
       Class filterClass = Class.forName(className);
-
-      Filter f = (Filter)filterClass.newInstance();
+      Constructor constructor = filterClass.getConstructor(params);
+      Filter f = (Filter)constructor.newInstance(values);
+      
+      //Filter f = (Filter)filterClass.newInstance();
       return f;
     }
     catch(Throwable t)
