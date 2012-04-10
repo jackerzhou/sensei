948c1ea616e9bc64d1c97d67d4153f9d8c16598e
wangdong <wangdong@xueqiu.com>
2012-04-10 16:49:21 +0800

    change custom filter params type usage

--- sensei-core/src/main/java/com/senseidb/search/query/filters/CustomFilterConstructor.java
+++ sensei-core/src/main/java/com/senseidb/search/query/filters/CustomFilterConstructor.java
@@ -2,6 +2,7 @@ package com.senseidb.search.query.filters;
 
 import java.lang.reflect.Constructor;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.lucene.search.Filter;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -10,7 +11,13 @@ public class CustomFilterConstructor extends FilterConstructor
 {
 
   public static final String FILTER_TYPE = "custom";
-  
+
+  public static final String PARAM_TYPE_INT = "int";
+  public static final String PARAM_TYPE_STRING = "string";
+  public static final String PARAM_TYPE_BOOLEAN = "boolean";
+  public static final String PARAM_TYPE_LONG = "long";
+  public static final String PARAM_TYPE_BYTES = "bytes";
+  public static final String PARAM_TYPE_DOUBLE = "double";
   
 //  // custom
 //  "custom" : {
@@ -31,15 +38,33 @@ public class CustomFilterConstructor extends FilterConstructor
       
       JSONArray paramArray = jsonObject.getJSONArray(PARAMS_PARAM);
       Class[] params = new Class[paramArray.length()];
-      for(int i = 0; i < paramArray.length(); i++) {
-    	  params[i] = Class.forName((String)paramArray.get(i));
-      }
-      
       JSONArray valueArray = jsonObject.getJSONArray(VALUES_PARAM);
       Object[] values = new Object[valueArray.length()];
-      for(int i = 0; i < valueArray.length(); i++) {
-    	  values[i] = params[i].cast(valueArray.get(i));
+      for(int i = 0; i < paramArray.length(); i++) {
+    	  String type = paramArray.getString(i);
+    	  if(type.equals(CustomFilterConstructor.PARAM_TYPE_INT)) {
+        	  params[i] = Integer.class;
+    		  values[i] = valueArray.getInt(i);
+    	  } else if(type.equals(CustomFilterConstructor.PARAM_TYPE_STRING)) {
+        	  params[i] = String.class;
+    		  values[i] = valueArray.getString(i);
+    	  } else if(type.equals(CustomFilterConstructor.PARAM_TYPE_BOOLEAN)) {
+        	  params[i] = Boolean.class;
+    		  values[i] = valueArray.getBoolean(i);
+    	  } else if(type.equals(CustomFilterConstructor.PARAM_TYPE_LONG)) {
+        	  params[i] = Long.class;
+    		  values[i] = valueArray.getLong(i);
+    	  } else if(type.equals(CustomFilterConstructor.PARAM_TYPE_BYTES)) {
+        	  params[i] = byte[].class;
+    	      String base64 = valueArray.getString(i);
+    	      byte[] bytes = Base64.decodeBase64(base64);
+    	      values[i] = bytes;
+    	  } else if(type.equals(CustomFilterConstructor.PARAM_TYPE_DOUBLE)) {
+        	  params[i] = Double.class;
+    		  values[i] = valueArray.getDouble(i);
+    	  }
       }
+      
       Class filterClass = Class.forName(className);
       Constructor constructor = filterClass.getConstructor(params);
       Filter f = (Filter)constructor.newInstance(values);
