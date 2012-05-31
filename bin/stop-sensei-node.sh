0ba2579a7c27e0a71c0683934d3a798e322da6a7
pawa <pawamaba@gmail.com>
2012-05-31 22:32:36 +0800

    copy kill.sh to stop-sensei-node.sh

--- bin/stop-sensei-node.sh
+++ bin/stop-sensei-node.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-kill -9 `cat /tmp/sensei-search-node.pid`
-rm /tmp/sensei-search-node.pid
+#usage="Usage: kill.sh <conf-dir>"
+
+PIDFILE=$1/logs/sensei-search-node.pid
+
+echo killing `cat $PIDFILE` and wait for it to die. could take a while
+kill `cat $PIDFILE`
+while ps -p `cat $PIDFILE`  > /dev/null; do sleep 1; done
+echo `cat $PIDFILE` killed
+echo remove ${PIDFILE}
+rm ${PIDFILE}
+echo done stop search node
