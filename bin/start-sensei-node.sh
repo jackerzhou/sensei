b4a6d50b75f072e6ef16d837cebad4f870914fd2
pawa <pawamaba@gmail.com>
2013-04-09 20:51:39 +0800

    add memory and disable UseGCOverheadLimit

--- bin/start-sensei-node.sh
+++ bin/start-sensei-node.sh
@@ -42,11 +42,12 @@ if [[ ! -d $logs ]]; then
   mkdir $logs
 fi
 
-HEAP_OPTS="-Xmx4096m -Xms2048m -XX:NewSize=1024m" # -d64 for 64-bit awesomeness
+HEAP_OPTS="-Xmx8192m -Xms2048m -XX:NewSize=1524m" # -d64 for 64-bit awesomeness
 #HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
 # HEAP_OPTS="-Xmx1024m -Xms512m -XX:NewSize=128m"
 # HEAP_OPTS="-Xmx512m -Xms256m -XX:NewSize=64m"
 # GC_OPTS="-verbosegc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
+GC_OPTS="-XX:-UseGCOverheadLimit"
 #JAVA_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=n"
 #GC_OPTS="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
 JAVA_OPTS="-server -d64"
